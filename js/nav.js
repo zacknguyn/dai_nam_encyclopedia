7fe4c2fc7d5666653fe985754fa15a31f458eb40
Le Tang Phu Quy <letangphuquy4@gmail.com>
2023-05-30 19:59:23 +0700

    Added curtain reveal effect for nav

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,26 @@
 const button = document.querySelector(".menu-btn");
-
+const nav = document.body.querySelector("nav");
 const dropdown = document.querySelector(".dropdown-menu");
-
-dropdown.style.display = "none"
+const main = document.querySelector("main");
 
 button.addEventListener("click", () => {
-    if (dropdown.style.display == "none") {
-        dropdown.style.display = "block";
+    dropdown.style.top = document.defaultView.getComputedStyle(nav).getPropertyValue("height");
+    let offsetBottom = 
+        parseInt(document.defaultView.getComputedStyle(dropdown).getPropertyValue("height"));
+    // console.log("Hasla vista baby, " + dropdown.style.top + "px and " + offsetBottom + "px");
+    if (nav.dataset.active == "false") {
+        nav.dataset.active = "true";
+        main.style.transform = "translateY(" + offsetBottom + "px)";
+        // dropdown.style.display = "block";
         button.style.transform = "rotate(90deg)";
-        button.style.transition = "all ease 0.5s";
         button.style.borderBottom = "5px solid #483420";
         button.style.borderRight = "5px solid #483420";
     }
-    else if (dropdown.style.display == "block") {
-        dropdown.style.display = "none";
+    else {
+        nav.dataset.active = "false";
+        main.style.transform = "translateY(0px)";
+        // dropdown.style.display = "none";
         button.style.transform = "rotate(0deg)";
-        button.style.transition = "all ease 0.5s";
         button.style.borderBottom = "none";
         button.style.borderRight = "none";
     }
