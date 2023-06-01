ec3c8028a3f839003d7e4abee9dc866c7e1b050e
Le Tang Phu Quy <letangphuquy4@gmail.com>
2023-06-01 23:55:28 +0700

    Enhanced nav bar + Installed Font Awesome + TO-DO: Add Carousel to the shop page Unify typeface and color pallete across pages

--- js/nav.js
+++ js/nav.js
@@ -6,14 +6,15 @@ const footer = document.querySelector("footer");
 
 button.addEventListener("click", () => {
     dropdown.style.top = document.defaultView.getComputedStyle(nav).getPropertyValue("height");
-    let offsetBottom = 
+    let dropdownHeight = 
         parseInt(document.defaultView.getComputedStyle(dropdown).getPropertyValue("height"));
+    document.body.style.setProperty("--dropdown-height", dropdownHeight + "px");
     // console.log("Hasla vista baby, " + dropdown.style.top + "px and " + offsetBottom + "px");
     if (nav.dataset.active == "false") {
         nav.dataset.active = "true";
-        main.style.transform = "translateY(" + offsetBottom + "px)";
-        footer.style.transform = "translateY(" + offsetBottom + "px)";
-        // dropdown.style.display = "block";
+
+        // main.style.transform = "translateY(" + dropdownHeight + "px)";
+        // footer.style.transform = "translateY(" + dropdownHeight + "px)";
         button.style.transform = "rotate(90deg)";
         button.style.borderBottom = "5px solid #483420";
         button.style.borderRight = "5px solid #483420";
@@ -21,8 +22,8 @@ button.addEventListener("click", () => {
     else {
         nav.dataset.active = "false";
         dropdown.style.zIndex = "-1";
-        main.style.transform = "translateY(0px)";
-        footer.style.transform = "translateY(0px)";
+        // main.style.transform = "translateY(0px)";
+        // footer.style.transform = "translateY(0px)";
         // dropdown.style.display = "none";
         button.style.transform = "rotate(0deg)";
         button.style.borderBottom = "none";
