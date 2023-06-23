03d96d6b41740b63b1597d45b1735cbd8e0313ff
zacknguyen <quangphongnguyen147@gmail.com>
2023-06-23 12:24:53 +0700

    Update 12:24PM 6/23/2023

--- js/searchbar.js
+++ js/searchbar.js
@@ -18,3 +18,24 @@ const search = () => {
         }
     }
 }
+
+const search_shop = () => {
+    const searchbox = document.getElementById("search-item-shop").value.toUpperCase()
+    const storeitems = document.getElementById("products-container")
+    const item = document.querySelectorAll(".product")
+    const productname = storeitems.getElementsByTagName("h3")
+
+    for(var i = 0; i < productname.length; i++) {
+        let match = item[i].getElementsByTagName('h3')[0];
+
+        if(match) {
+            let textValue = match.textContent || match.innerHTML
+
+            if(textValue.toUpperCase().indexOf(searchbox) > -1) {
+                item[i].style.display = "";
+            } else {
+                item[i].style.display = "none";
+            }
+        }
+    }
+}
