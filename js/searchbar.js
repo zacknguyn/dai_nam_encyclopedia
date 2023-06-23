1287145e3b6527792d4d60a161cab87e23ea4507
Le Tang Phu Quy <letangphuquy4@gmail.com>
2023-06-24 01:22:37 +0700

    Fixed various UI bugs

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,41 +1,40 @@
-const search = () => {
-    const searchbox = document.getElementById("search-item").value.toUpperCase()
-    const storeitems = document.getElementById("blog-container")
-    const item = document.querySelectorAll(".blog-box")
-    const blogname = storeitems.getElementsByTagName("h3")
+const generic_search = (searchboxID, itemsListID, itemSelector) => {
+    const searchbox = document.getElementById(searchboxID).value.toUpperCase()
+    const itemsList = document.getElementById(itemsListID)
+    const item = document.querySelectorAll(itemSelector)
+    const productname = itemsList.getElementsByTagName("h3")
 
-    for(var i = 0; i < blogname.length; i++) {
-        let match = item[i].getElementsByTagName('h3')[0];
+    let countMatch = 0;
+    for(var i = 0; i < productname.length; i++) {
+        let content = item[i].getElementsByTagName('h3')[0];
 
-        if(match) {
-            let textValue = match.textContent || match.innerHTML
+        if (content) {
+            let textValue = content.textContent || content.innerHTML
 
             if(textValue.toUpperCase().indexOf(searchbox) > -1) {
                 item[i].style.display = "";
+                ++countMatch;
             } else {
                 item[i].style.display = "none";
             }
         }
+    }
+    console.log(countMatch);
+    let noMatch = document.getElementById("no-result");
+    if (countMatch == 0) {
+        noMatch.style.display = "block";
+        document.documentElement.style.background = "var(--bg-color)";
+    }
+    else {
+        noMatch.style.display = "none";
+        document.documentElement.style.background = "var(--curtain-color)";
     }
 }
 
+const search = () => {
+    generic_search("search-item", "blog-container", ".blog-box");
+}
+
 const search_shop = () => {
-    const searchbox = document.getElementById("search-item-shop").value.toUpperCase()
-    const storeitems = document.getElementById("products-list")
-    const item = document.querySelectorAll("#products-list .product")
-    const productname = storeitems.getElementsByTagName("h3")
-
-    for(var i = 0; i < productname.length; i++) {
-        let match = item[i].getElementsByTagName('h3')[0];
-
-        if(match) {
-            let textValue = match.textContent || match.innerHTML
-
-            if(textValue.toUpperCase().indexOf(searchbox) > -1) {
-                item[i].style.display = "";
-            } else {
-                item[i].style.display = "none";
-            }
-        }
-    }
+    generic_search("search-item-shop", "products-list", "#products-list .product");
 }
